84a7ba5d6614bb91b401b6aecf9520e8c17db72c
Bobur Jumaniyazov <bobur_jumaniyazov@epam.com>
2023-10-26 14:19:50 +0500

    Comment removed

--- src/main/java/dasturlash/uz/Main.java
+++ src/main/java/dasturlash/uz/Main.java
@@ -12,15 +12,15 @@ public class Main {
         ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
         StudentRepository studentRepository = (StudentRepository) context.getBean("studentRepository");
 
-//        StudentDTO student1 = new StudentDTO("Ali", "Aliyev", LocalDateTime.now());
-//        StudentDTO student2 = new StudentDTO("Vali", "Valiyev", LocalDateTime.now());
-//        StudentDTO student3 = new StudentDTO("Toshmat", "Toshmatov", LocalDateTime.now());
-//        StudentDTO student4 = new StudentDTO("Eshmat", "Eshmatov", LocalDateTime.now());
+        StudentDTO student1 = new StudentDTO("Ali", "Aliyev", LocalDateTime.now());
+        StudentDTO student2 = new StudentDTO("Vali", "Valiyev", LocalDateTime.now());
+        StudentDTO student3 = new StudentDTO("Toshmat", "Toshmatov", LocalDateTime.now());
+        StudentDTO student4 = new StudentDTO("Eshmat", "Eshmatov", LocalDateTime.now());
 
-//        studentRepository.save(student1);
-//        studentRepository.save(student2);
-//        studentRepository.save(student3);
-//        studentRepository.save(student4);
+        studentRepository.save(student1);
+        studentRepository.save(student2);
+        studentRepository.save(student3);
+        studentRepository.save(student4);
 
         List<StudentDTO> studentList = studentRepository.getStudentList();
         studentList.forEach(dto -> System.out.println(dto));
